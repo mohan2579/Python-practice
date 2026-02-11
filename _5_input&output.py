d0517574e9e89a522876b044af5439f5ef543cbe
Mohan <mohanrr2579@gmail.com>
2026-02-11 15:40:57 +0530

    Added about input,output,comments file

---
+++ _5_input&output.py
@@ -0,0 +1,11 @@
+# Take input from user
+""" 'input()' allows flexibility to take input from the user. Reads a line of input as a string. """
+username = input() # Ajay
+
+# Printing the Output
+""" 'print()' function prints the message to the screen or any other standard output device. """
+print(username) # Ajay
+
+# Comments
+""" Comment starts with a hash '#'. it can be written in its own line next to a statement of code. """
+# This is comment
