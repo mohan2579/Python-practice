1e2779deb03ab03d07f25730b18f00d6588dafc9
Mohan <mohanrr2579@gmail.com>
2026-02-11 20:02:35 +0530

    Added string method and i stopped here

---
+++ _6_string_methods.py
@@ -0,0 +1,18 @@
+# String Methods
+
+# isdigit()
+""" Usage: Gives True if al teh characters are digits. Otherwise, False. """
+result = "123".isdigit()
+print(result) # True
+result = "123a".isdigit()
+print(result) # False
+
+# strip()
+""" Usage: Removes all the leading and trailing spaces from the string. """
+result = "   hello   ".strip()
+print(result) # hello
+
+# strip() with separator
+""" Usage: we can also specify separator(string) that need to be removed. """
+
+# IMPORTANT: i have stoped here for now
