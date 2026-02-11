e31e5f964583f6746836f2ec566ac6731320d4e1
Mohan <mohanrr2579@gmail.com>
2026-02-11 12:50:22 +0530

    added two practice files

---
+++ _1_data_types.py
@@ -0,0 +1,17 @@
+# Data types
+"""
+In programming languages, every value or data has an associated type to it known as data type.
+"""
+
+# String
+a = "Hello"
+
+# interger
+a = 124
+
+# float
+a = 12.3454
+
+# Boolean
+b = True or False
+
---
+++ _2_conditional_statements.py
@@ -0,0 +1,16 @@
+# Conditional statements
+
+"""
+Conditional statement allows you to execute a block of code only when a specific condition is true
+"""
+# If condition
+if True:
+    print("If block")
+    print("Inside if")
+
+# If Else
+
+if False:
+    pirnt() # it will not go inside if condition is false
+else:
+    print("It was else")
