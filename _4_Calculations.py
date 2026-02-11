f77156991f6d3daa57e5f926bc6ae0b949d167f0
Mohan <mohanrr2579@gmail.com>
2026-02-11 15:17:59 +0530

    Added Calculation file

---
+++ _4_Calculations.py
@@ -0,0 +1,28 @@
+# Addition
+""" Addition is denoted by '+' sign. """
+print(2 + 2.5) # 4.5
+print(1 + 1.5) # 2.5
+
+# Subtraction
+""" Subrtraction is denoted by '-' sign """
+print(5 -2) # 3
+
+# Multiplication
+""" Multiplication is denoted by '*' sign """
+print(2 * 5) # 10
+
+# Division
+""" Division is denoted by '/' sign """
+print(10 / 2) # 5.0
+
+# Modulus
+""" To find the remainder, we use the Modulus operator '%' sign. """
+print( 7 % 2) # 1
+
+# Exponent
+""" To find 'a' power 'b', we use exponent operator '**' sign. """
+print(2 ** 5) # 32
+
+# Floor division
+""" To find an integral part of quotient we use Floor Divsion Operator '//' sign. """
+print (13 // 5) # 2
