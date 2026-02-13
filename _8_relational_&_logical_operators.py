40a623f00c9d3f7e6d4a5459bc4e618daa99afd6
Mohan <mohanrr2579@gmail.com>
2026-02-13 19:55:47 +0530

    added relational and logical operators

---
+++ _8_relational_&_logical_operators.py
@@ -0,0 +1,21 @@
+# Relational operators
+"""
+Relational Operators are used to comparing values. Gives 'True' or 'False' as the result of a comparsion.
+>   👉 is greater than
+<   👉 is less than
+==  👉 is equal to
+<=  👉 less than or equal to
+>=  👉 greater than or equals to
+!=  👉 is not equals to
+"""
+
+# Logical Operators
+"""
+Logical Operators are used to performing logical operations on boolean values. Gives 'True' or 'False' as a
+result
+
+and | or | not
+
+imagine the logical truth table every time.
+
+"""
